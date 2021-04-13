16b60b89789c70ae49b6f474c3a57958b1f351a6
kromate <akpananthony33@gmail.com>
2021-04-13 16:09:37 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Preloader v-if="user === false" />
+    <Preloader v-if="user == false" />
     <SignUp v-else-if="pendingAccount !== false" :userData="pendingAccount" />
     <router-view v-else :user="user" />
   </div>
