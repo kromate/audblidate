4855b9bb5a9e8d4957000dcbec02fada3e44c618
kromate <akpananthony33@gmail.com>
2021-04-13 22:47:20 +0100

    update

--- server/server.js
+++ server/server.js
@@ -69,7 +69,7 @@ io.on("connection", (socket) => {
 // set static folder
 app.use(express.static(path.join(__dirname, "./public")));
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
